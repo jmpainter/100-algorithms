c8737862ed7ad2b5399b446bf59543c2161432b4
jmpainter <jpainter4@gmail.com>
2019-02-19 11:23:45 -0800

    added checkPalindrome

--- checkPalindrome/checkPalindrome.ts
+++ checkPalindrome/checkPalindrome.ts
@@ -1,5 +1,19 @@
-function checkPalindrome(inputString: string): boolean {
-
+function checkPalindrome(inputString) {
+  const map = {};
+  for (let char of inputString) {
+    if (!map[char]) {
+      map[char] = 1;
+    } else {
+      map[char]++;
+    }
+  }
+  let oddCount = 0;
+  for (let key in map) {
+    if (map[key] % 2 === 1) {
+      oddCount++;
+    }
+  }
+  return oddCount > 1 ? false : true;
 }
 
 console.log(checkPalindrome('aabaa'));
