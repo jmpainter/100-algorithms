da22dcef966e2edea93ec1677616158839019dfd
jmpainter <jpainter4@gmail.com>
2019-02-19 11:16:13 -0800

    added characterParity

--- characterParity/characterParity.ts
+++ characterParity/characterParity.ts
@@ -1,7 +1,14 @@
-function characterParity(symbol: string): string {
-   
+function characterParity(symbol) {
+  debugger;
+  if (isNaN(parseInt(symbol))) {
+    return 'not a digit';
+  } else if (parseInt(symbol) % 2 === 0) {
+    return 'even';
+  } else if (parseInt(symbol) % 2 === 1) {
+    return 'odd';
+  }
 }
 
-console.log(characterParity('5'))
-console.log(characterParity('8'))
-console.log(characterParity('q'))
+console.log(characterParity('5'));
+console.log(characterParity('8'));
+console.log(characterParity('q'));
