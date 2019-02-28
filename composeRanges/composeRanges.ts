43cb9583e47c45112dfaeab2cfc6667453131dc5
jmpainter <jpainter4@gmail.com>
2019-02-27 22:18:32 -0800

    added composeRanges

--- composeRanges/composeRanges.ts
+++ composeRanges/composeRanges.ts
@@ -1,6 +1,20 @@
-function composeRanges(nums: number[]): string[] {
+function composeRanges(nums) {
+  const ranges = [[nums[0], nums[0]]];
+  for (let i = 1; i < nums.length; i++) {
+    if (ranges[ranges.length - 1][1] + 1 === nums[i]) {
+      ranges[ranges.length - 1][1] = nums[i];
+    } else {
+      ranges.push([nums[i], nums[i]]);
+    }
+  }
 
+  return ranges.map(range => {
+    if (range[0] === range[1]) {
+      return range[0].toString();
+    } else {
+      return range[0].toString() + '->' + range[1].toString();
+    }
+  });
 }
 
-
-console.log(composeRanges([-1, 0, 1, 2, 6, 7, 9]));
+console.log(composeRanges([-1, 1, 2, 6, 7, 9]));
