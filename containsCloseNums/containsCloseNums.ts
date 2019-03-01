b202c08d25fdb4900a060c2a03ed3a3cc51d16ae
jmpainter <jpainter4@gmail.com>
2019-02-28 19:43:49 -0800

    added containsCloseNums

--- containsCloseNums/containsCloseNums.ts
+++ containsCloseNums/containsCloseNums.ts
@@ -1,5 +1,12 @@
-function containsCloseNums(nums: number[], k: number): boolean {
-
+function containsCloseNums(nums, k) {
+  for (let i = 0; i < nums.length; i++) {
+    for (let j = i + 1; j < i + k + 1; j++) {
+      if (nums[i] === nums[j]) {
+        return true;
+      }
+    }
+  }
+  return false;
 }
 
 console.log(containsCloseNums([0, 1, 2, 3, 5, 2], 3));
