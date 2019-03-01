51e2ff5f35eec650eccc0121438d90f8f352289a
jmpainter <jpainter4@gmail.com>
2019-03-01 15:16:45 -0800

    added convertString

--- convertString/convertString.ts
+++ convertString/convertString.ts
@@ -1,5 +1,16 @@
-function convertString(s: string, t: string): boolean {
-
+function convertString(s, t) {
+  let ptr1 = 0;
+  let ptr2 = 0;
+  while (ptr2 < t.length) {
+    const foundIndex = s.indexOf(t.charAt(ptr2), ptr1);
+    if (foundIndex === -1) {
+      return false;
+    } else {
+      ptr1 = foundIndex + 1;
+      ptr2++;
+    }
+  }
+  return true;
 }
 
 console.log(convertString('ceoydefthf5iyg5h5yts', 'codefights'));
