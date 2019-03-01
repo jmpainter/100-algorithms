7cfe9f2e8fe09d676471c11f7ed4a89c1c482ef2
jmpainter <jpainter4@gmail.com>
2019-02-28 19:37:40 -0800

    added confirmEnding

--- confirmEnding/confirmEnding.ts
+++ confirmEnding/confirmEnding.ts
@@ -1,6 +1,9 @@
-function confirmEnding(str: string, target: string) {
-   
+function confirmEnding(str, target) {
+  if (str.slice(-target.length) === target) {
+    return true;
+  }
+  return false;
 }
 
-console.log(confirmEnding("Abstraction", "action"));
-console.log(confirmEnding("Open sesame", "pen"));
+console.log(confirmEnding('Abstraction', 'action'));
+console.log(confirmEnding('Open sesame', 'pen'));
