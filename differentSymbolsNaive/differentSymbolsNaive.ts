d31599a573581949c8747f64c0d293f195fad3e8
jmpainter <jpainter4@gmail.com>
2019-03-02 16:56:35 -0800

    update differentSymbols

--- differentSymbolsNaive/differentSymbolsNaive.ts
+++ differentSymbolsNaive/differentSymbolsNaive.ts
@@ -1,5 +1,22 @@
-function differentSymbolsNaive(s: string): number {
- 
+function differentSymbolsNaive(s) {
+  const map = {};
+  for (let char of s) {
+    if (!map[char]) {
+      map[char] = 1;
+    } else {
+      map[char]++;
+    }
+  }
+  return Object.keys(map).length;
+}
+
+function differentSymbolsNaive2(s) {
+  const uniqueChars = new Set();
+  for (let char of s) {
+    uniqueChars.add(char);
+  }
+  return uniqueChars.size;
 }
 
 console.log(differentSymbolsNaive('cabca'));
+console.log(differentSymbolsNaive2('cabca'));
