dd6727ec296f3b5e59156b2e88022fdd97fdd9a0
jmpainter <jpainter4@gmail.com>
2019-03-02 16:45:13 -0800

    added crossingSum

--- crossingSum/crossingSum.ts
+++ crossingSum/crossingSum.ts
@@ -1,7 +1,14 @@
-function crossingSum(matrix: number[][], a: number, b: number): number {
-  
+function crossingSum(matrix, row, col) {
+  const rowSum = matrix[row].reduce((acc, ele) => acc + ele);
+
+  let colSum = 0;
+
+  for (let i = 0; i < matrix.length; i++) {
+    if (i !== row) {
+      colSum += matrix[i][col];
+    }
+  }
+  return rowSum + colSum;
 }
 
-console.log(crossingSum([[1, 1, 1, 1], 
-    [2, 2, 2, 2], 
-    [3, 3, 3, 3]], 1, 3));
+console.log(crossingSum([[1, 1, 1, 1], [2, 2, 2, 2], [3, 3, 3, 3]], 1, 3));
