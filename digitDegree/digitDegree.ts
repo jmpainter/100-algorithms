6e4f697ce4935e16a253ac828f6af5859a0f9966
jmpainter <jpainter4@gmail.com>
2019-03-02 17:06:50 -0800

    added digitDegree

--- digitDegree/digitDegree.ts
+++ digitDegree/digitDegree.ts
@@ -1,7 +1,18 @@
-function digitDegree(n: number): number {
-  
+function digitDegree(n) {
+  let steps = 0;
+
+  while (n.toString().length > 1) {
+    steps++;
+    n = n
+      .toString()
+      .split('')
+      .map(ele => parseInt(ele))
+      .reduce((acc, ele) => acc + ele);
+  }
+  return steps;
 }
 
 console.log(digitDegree(5));
 console.log(digitDegree(10));
 console.log(digitDegree(91));
+console.log(digitDegree(3242323333));
