2da0b133fe9459985b0ea1e305e38bdc0e58c76d
jmpainter <jpainter4@gmail.com>
2019-03-04 15:21:49 -0800

    added electionWinners

--- electionWinners/electionWinners.ts
+++ electionWinners/electionWinners.ts
@@ -1,5 +1,13 @@
-function electionsWinners(votes: number[], k: number): number {
-
+function electionsWinners(votes, k) {
+  votes = votes.sort((a, b) => b - a);
+  let max = votes[0];
+  let couldWin = 1;
+  for (let i = 1; i < votes.length; i++) {
+    if (votes[i] > max - k) {
+      couldWin++;
+    }
+  }
+  return couldWin;
 }
 
-console.log(electionsWinners([2, 3, 5, 2], 3));
+console.log(electionsWinners([2, 3, 5, 2], 0));
