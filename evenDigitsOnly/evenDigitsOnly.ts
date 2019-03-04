a54615a4ec456ea5f96861fc5a7cfe08b8fbc59b
jmpainter <jpainter4@gmail.com>
2019-03-04 15:29:06 -0800

    added eveDigitsOnly

--- evenDigitsOnly/evenDigitsOnly.ts
+++ evenDigitsOnly/evenDigitsOnly.ts
@@ -1,5 +1,10 @@
-function evenDigitsOnly(n: number): boolean {
+function evenDigitsOnly(n) {
+  const digits = n
+    .toString()
+    .split('')
+    .map(num => parseInt(num));
 
+  return digits.every(num => num % 2 === 0);
 }
 
 console.log(evenDigitsOnly(248622));
