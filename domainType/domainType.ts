c261303346d714321f5dd2971a30c59a033e79a8
jmpainter <jpainter4@gmail.com>
2019-03-04 15:04:52 -0800

    added domainType

--- domainType/domainType.ts
+++ domainType/domainType.ts
@@ -1,5 +1,18 @@
-function domainType(domains: string[]): string[] {
-
+function domainType(domains) {
+  const map = {
+    com: 'commmercial',
+    org: 'organization',
+    net: 'network',
+    info: 'information'
+  };
+  const result = [];
+  domains.forEach(domain => {
+    const top = domain.slice(domain.lastIndexOf('.') + 1);
+    result.push(map[top]);
+  });
+  return result;
 }
 
-console.log(domainType(["en.wiki.org", "codefights.com", "happy.net", "code.info"]));
+console.log(
+  domainType(['en.wiki.org', 'codefights.com', 'happy.net', 'code.info'])
+);
