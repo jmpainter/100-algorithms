c7ffea23c041b73b0af7ffbf82173eb824d71c7e
jmpainter <jpainter4@gmail.com>
2019-03-19 17:37:15 -0700

    added extractMatrixColumn

--- extractMatrixColumn/extractMatrixColumn.ts
+++ extractMatrixColumn/extractMatrixColumn.ts
@@ -1,5 +1,9 @@
-function extractMatrixColumn(matrix: number[][], column: number): number[] {
-
+function extractMatrixColumn(matrix, column) {
+  const retArray = [];
+  matrix.forEach((row, index) => {
+    retArray.push(matrix[index][column]);
+  });
+  return retArray;
 }
 
 console.log(extractMatrixColumn([[1, 1, 1, 2], [0, 5, 0, 4], [2, 1, 3, 6]], 2));
