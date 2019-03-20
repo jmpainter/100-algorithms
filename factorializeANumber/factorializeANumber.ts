152e2f739f5e5d72b73fdd9f1984936236d7ae78
jmpainter <jpainter4@gmail.com>
2019-03-19 17:40:26 -0700

    added factorializeNumber

--- factorializeANumber/factorializeANumber.ts
+++ factorializeANumber/factorializeANumber.ts
@@ -1,5 +1,9 @@
-function factorializeANumber(num: number): number {
-
+function factorializeANumber(num) {
+  let factorial = 1;
+  for (let i = 1; i <= num; i++) {
+    factorial *= i;
+  }
+  return factorial;
 }
 
 console.log(factorializeANumber(5));
