84951e4be4bb53b332b0fd07d7a4a68d3c37c5b4
jmpainter <jpainter4@gmail.com>
2019-03-19 17:49:07 -0700

    added fancyRide

--- fancyRide/fancyRide.ts
+++ fancyRide/fancyRide.ts
@@ -1,5 +1,12 @@
-function fancyRide(l: number, fares: number[]): string {
-
+function fancyRide(l, fares) {
+  const types = ['UberX', 'UberXL', 'UberPlus', 'UberBlack', 'UberSUV'];
+  for (let i = fares.length - 1; i >= 0; i--) {
+    if (fares[i] * l <= 20) {
+      return types[i];
+    }
+  }
+  return 'Cannot afford a ride';
 }
 
 console.log(fancyRide(30, [0.3, 0.5, 0.7, 1, 1.3]));
+console.log(fancyRide(2, [0.3, 0.5, 0.7, 1, 1.3]));
