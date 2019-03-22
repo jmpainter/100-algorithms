7026b02eeadbd3cf2882f10f614e60d990fd52d3
jmpainter <jpainter4@gmail.com>
2019-03-21 20:04:31 -0700

    added findClosestPair

--- fermactor/fermactor.ts
+++ fermactor/fermactor.ts
@@ -1,5 +1,13 @@
-function fermactor(n: number): number[] {
-
+function fermactor(n) {
+  let a = 1;
+  while (true) {
+    const result = Math.pow(a, 2) - n;
+    if (Math.sqrt(result) % 1 === 0) {
+      return [a, Math.sqrt(result)];
+    }
+    a++;
+  }
 }
 
 console.log(fermactor(15));
+console.log(fermactor(21));
--- findClosestPair/findClosestPair.ts
+++ findClosestPair/findClosestPair.ts
@@ -1,5 +1,17 @@
-function findClosestPair(numbers: number[], sum: number): number {
-   
+function findClosestPair(numbers, sum) {
+  const distances = [];
+  for (let i = 0; i < numbers.length - 1; i++) {
+    for (let j = i + 1; j < numbers.length - 1; j++) {
+      if (numbers[i] + numbers[j] === sum) {
+        distances.push(j - i);
+      }
+    }
+  }
+  if (distances.length === 0) {
+    return -1;
+  } else {
+    return distances.sort()[0];
+  }
 }
 
 console.log(findClosestPair([1, 0, 2, 4, 3, 0], 5));
