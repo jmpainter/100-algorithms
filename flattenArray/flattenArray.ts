4228366066b5b1e22acbb4ec18dff6bab236b188
jmpainter <jpainter4@gmail.com>
2019-03-22 16:38:44 -0700

    added flattenArray.js

--- flattenArray/flattenArray.ts
+++ flattenArray/flattenArray.ts
@@ -1,6 +1,16 @@
-function flattenArray(arr: any[]): any[] {
+function flattenArray(arr) {
+  const result = [];
 
+  while (arr.length > 0) {
+    const item = arr.pop();
+    if (!Array.isArray(item)) {
+      result.push(item);
+    } else {
+      arr.push(...item);
+    }
+  }
+  return result;
 }
 
-console.log(flattenArray([[["a"]], [["b"]]]));
+console.log(flattenArray([[['a']], [['b']]]));
 console.log(flattenArray([1, [2], [3, [[4]]]]));
