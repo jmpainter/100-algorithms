dd20151e5dd7aca1a39f0c99a0a18c65e7af314f
jmpainter <jpainter4@gmail.com>
2019-03-22 16:48:59 -0700

    added houseNumbersSum

--- houseNumbersSum/houseNumbersSum.ts
+++ houseNumbersSum/houseNumbersSum.ts
@@ -1,5 +1,12 @@
-function houseNumbersSum(inputArray: number[]): number {
-
+function houseNumbersSum(inputArray) {
+  let sum = 0;
+  for (let number of inputArray) {
+    if (number === 0) {
+      return sum;
+    } else {
+      sum += number;
+    }
+  }
 }
 
 console.log(houseNumbersSum([5, 1, 2, 3, 0, 1, 5, 0, 2]));
