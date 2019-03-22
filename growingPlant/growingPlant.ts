3f20376cc665c499d41abba0749f47236d86522a
jmpainter <jpainter4@gmail.com>
2019-03-22 16:46:14 -0700

    added growingPlants

--- growingPlant/growingPlant.ts
+++ growingPlant/growingPlant.ts
@@ -1,5 +1,17 @@
-function growingPlant(upSpeed: number, downSpeed: number, desiredHeight: number): number {
-
+function growingPlant(upSpeed, downSpeed, desiredHeight) {
+  if (downSpeed > upSpeed) {
+    return -1;
+  }
+  let currHeight = 0;
+  let days = 0;
+  while (true) {
+    currHeight = currHeight + upSpeed;
+    currHeight = currHeight - downSpeed;
+    if (currHeight >= desiredHeight) {
+      return days;
+    }
+    days++;
+  }
 }
 
-console.log(growingPlant(100, 10, 910))
+console.log(growingPlant(100, 10, 910));
