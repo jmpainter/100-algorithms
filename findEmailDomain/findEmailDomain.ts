9ee5c96685b7cffb8da0a4e56b8d5d8948ee962f
jmpainter <jpainter4@gmail.com>
2019-03-22 16:26:52 -0700

    added findEmailDomains

--- findEmailDomain/findEmailDomain.ts
+++ findEmailDomain/findEmailDomain.ts
@@ -1,6 +1,7 @@
-function findEmailDomain(address: string): string {
-
+function findEmailDomain(address) {
+  const index = address.lastIndexOf('@');
+  return address.slice(index + 1);
 }
 
 console.log(findEmailDomain('prettyandsimple@example.com'));
-console.log(findEmailDomain('<>[]:,;@\"!#$%&*+-/=?^_{}| ~.a\"@example.org'));
+console.log(findEmailDomain('<>[]:,;@"!#$%&*+-/=?^_{}| ~.a"@example.org'));
