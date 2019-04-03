2e7aec9bec8502471ee35e2771f51c0acb1bdf51
jmpainter <jpainter4@gmail.com>
2019-04-02 19:03:45 -0700

    added incorrectPasswordAttempts.ts

--- incorrectPasswordAttempts/incorrectPasswordAttempts.ts
+++ incorrectPasswordAttempts/incorrectPasswordAttempts.ts
@@ -1,5 +1,27 @@
-function incorrectPasscodeAttempts(passcode: string, attempts: string[]): boolean {
-
+function incorrectPasscodeAttempts(passcode, attempts) {
+  let failedAttempts = 0;
+  attempts.forEach(attempt => {
+    if (attempt.valueOf() === passcode.valueOf()) {
+      failedAttempts++;
+    }
+  });
+  console.log(failedAttempts);
+  return failedAttempts > 0 ? true : false;
 }
 
-console.log(incorrectPasscodeAttempts('1111', ["1111", "4444", "9999", "3333", "8888", "2222", "7777", "0000", "6666", "7285", "5555", "1111"]));
+console.log(
+  incorrectPasscodeAttempts('1111', [
+    '1111',
+    '4444',
+    '9999',
+    '3333',
+    '8888',
+    '2222',
+    '7777',
+    '0000',
+    '6666',
+    '7285',
+    '5555',
+    '1111'
+  ])
+);
