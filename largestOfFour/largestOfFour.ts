d90c0d71f6b1cffcc0c70a6ed360d2c940146840
jmpainter <jpainter4@gmail.com>
2019-04-08 19:59:35 -0700

    added largestOfFour

--- largestOfFour/largestOfFour.ts
+++ largestOfFour/largestOfFour.ts
@@ -1,6 +1,24 @@
-function largestOfFour(nums: number[][]): number[] {
-
+function largestOfFour(nums) {
+  const result = [];
+  nums.forEach(numArray => {
+    result.push(numArray.sort((a, b) => b - a)[0]);
+  });
+  return result;
 }
 
-console.log(largestOfFour([[4, 5, 1, 3], [13, 27, 18, 26], [32, 35, 37, 39], [1000, 1001, 857, 1]]) );
-console.log(largestOfFour([[4, 9, 1, 3], [13, 35, 18, 26], [32, 35, 97, 39], [1000000, 1001, 857, 1]]));
+console.log(
+  largestOfFour([
+    [4, 5, 1, 3],
+    [13, 27, 18, 26],
+    [32, 35, 37, 39],
+    [1000, 1001, 857, 1]
+  ])
+);
+console.log(
+  largestOfFour([
+    [4, 9, 1, 3],
+    [13, 35, 18, 26],
+    [32, 35, 97, 39],
+    [1000000, 1001, 857, 1]
+  ])
+);
