d00d3bade8152557cec693aad275f0f7898d176c
jmpainter <jpainter4@gmail.com>
2019-04-08 19:50:25 -0700

    added isTandemRepeat

--- isTandemRepeat/isTandemRepeat.ts
+++ isTandemRepeat/isTandemRepeat.ts
@@ -1,7 +1,15 @@
-function isTandemRepeat(inputString: string): boolean {
-  
+function isTandemRepeat(inputString) {
+  if (inputString.length % 2 === 1) {
+    return false;
+  }
+  return (
+    inputString.slice(0, inputString.length / 2) ===
+    inputString.slice(-inputString.length / 2)
+  );
 }
 
-console.log(isTandemRepeat('tandemtandem'))
-console.log(isTandemRepeat('qqq'))
-console.log(isTandemRepeat('2w2ww'))
+console.log(isTandemRepeat('tandemtandem'));
+console.log(isTandemRepeat('qqq'));
+console.log(isTandemRepeat('qqqt'));
+console.log(isTandemRepeat('2w2ww'));
+console.log(isTandemRepeat('frogfrog'));
