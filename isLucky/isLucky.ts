9b430954b585d3c1eaf2cb17a5560c632f989b61
jmpainter <jpainter4@gmail.com>
2019-04-08 19:36:20 -0700

    added isLucky

--- isLucky/isLucky.ts
+++ isLucky/isLucky.ts
@@ -1,5 +1,12 @@
-function isLucky(n: number): boolean {
-    
+function isLucky(n) {
+  const numArray = n
+    .toString()
+    .split('')
+    .map(digitString => parseInt(digitString, 10));
+  const half = numArray.length / 2;
+  const firstHalfSum = numArray.slice(0, half).reduce((acc, ele) => acc + ele);
+  const secondHalfSum = numArray.slice(-half).reduce((acc, ele) => acc + ele);
+  return firstHalfSum === secondHalfSum;
 }
 
 console.log(isLucky(1230));
