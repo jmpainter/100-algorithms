69233c15f1b129fbdadecff9c8b2ab779a09cd26
jmpainter <jpainter4@gmail.com>
2019-04-23 20:05:04 -0700

    added lateRide.ts

--- lateRide/lateRide.ts
+++ lateRide/lateRide.ts
@@ -1,6 +1,13 @@
-function lateRide(n: number): number {
+function lateRide(n) {
+  const hours = Math.floor(n / 60);
+  const minutes = n % 60;
 
+  const time = hours.toString() + minutes.toString()
+  const total = time.split('').map(number => parseInt(number)).reduce((acc, ele) => acc + ele);
+  return total;
 }
+
+
 
 console.log(lateRide(240));
 console.log(lateRide(808));
